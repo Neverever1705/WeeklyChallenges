100300e5e0c79db5dddd8b089fd2fbb82ea6c660
Neverever_1705 <164153004+Neverever1705@users.noreply.github.com>
2025-09-11 15:11:09 +0200

    Create ValidHexCode.java

---
+++ ValidHexCode.java
@@ -0,0 +1,27 @@
+//Create a function that determines whether a string is a valid hex code.
+
+//A hex code must begin with a pound key # and is exactly 6 characters in length.
+
+//Each character must be a digit from 0-9 or an alphabetic character from A-F. All alphabetic characters may be uppercase or lowercase.
+
+public class ValidHexCode {
+
+
+    public boolean isValidHexCode(String hexCode) {
+        if (hexCode.charAt(0) != '#' || hexCode.trim().length() > 7)
+            return false;
+        for (int i = 1 ; i < hexCode.length(); i++) {
+            switch (hexCode.toUpperCase().charAt(i)){
+                case 'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9' ->{
+
+                }
+                default -> {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+
+    }
+}
